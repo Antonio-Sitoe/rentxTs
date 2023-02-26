4e7b87bb3a4069e99a77f06f7cb5f1c1cafb3f90
Antonio Sitoe <antoniositoehl@gmail.com>
2023-02-26 08:13:21 +0200

    add

---
+++ App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { StatusBar } from "expo-status-bar";
+import Home from "./src/Screens/Home";
+
+export default function App() {
+  return (
+    <>
+      <StatusBar />
+      <Home />
+    </>
+  );
+}
