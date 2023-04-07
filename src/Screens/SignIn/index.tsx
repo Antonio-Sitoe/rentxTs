48f6fa6d02bf8158f743b53795b5eda3c8f0094b
Antonio Sitoe <antonio.sitoe@void.co.mz>
2023-04-07 21:13:46 +0200

    adidionado libs

--- src/Screens/SignIn/index.tsx
+++ src/Screens/SignIn/index.tsx
@@ -1,50 +1,96 @@
+import React from "react";
 import { Container, Header, Title, SubTitle, Form, Footer } from "./styles";
-import { StatusBar } from "react-native";
-import { Button } from "../../components/Button";
+import {
+  StatusBar,
+  KeyboardAvoidingView,
+  TouchableWithoutFeedback,
+  Keyboard,
+  Alert,
+} from "react-native";
 import { Input } from "../../components/Input";
-import theme from "../../styles/theme";
+import { Button } from "../../components/Button";
 import { PasswordInput } from "../../components/PasswordInput";
+import * as Yup from "yup";
+
+const schema = Yup.object().shape({
+  email: Yup.string()
+    .required("E-mail obrigatório")
+    .email("Digite um e-mail válido"),
+  password: Yup.string().required("A senha é obrigatória"),
+});
+
 export function SignIn() {
+  const [email, setEmail] = React.useState("");
+  const [password, setPassword] = React.useState("");
+
+  async function handleSignIn() {
+    try {
+      await schema.validate({ email, password });
+
+      // signIn({ email, password });
+    } catch (error) {
+      if (error instanceof Yup.ValidationError) {
+        Alert.alert("Opa", error.message);
+      } else {
+        Alert.alert(
+          "Erro na autenticação",
+          "Ocorreu um erro ao fazer login, verifique as credenciais"
+        );
+      }
+    }
+  }
+
   return (
-    <Container>
-      <StatusBar
-        barStyle="dark-content"
-        backgroundColor="transparent"
-        translucent
-      />
-      <Header>
-        <Title>Estamos {"\n"} quase la.</Title>
-        <SubTitle>
-          Faca seu login para comecar {"\n"}
-          uma experiencia incrivel.
-        </SubTitle>
-      </Header>
-      <Form>
-        <Input
-          iconName="mail"
-          keyboardType="email-address"
-          placeholder="E-mail"
-          autoCapitalize="none"
-          autoCorrect={false}
-        />
-        <PasswordInput
-          iconName="lock"
-          placeholder="Senha"
-          // onChangeText={setPassword}
-          // value={password}
-        />
-      </Form>
-      <Footer>
-        <Button title="Login" enabled loading={false} onPress={() => {}} />
-        <Button
-          title="Criar conta gratuita"
-          color={theme.colors.background_secondary}
-          light={true}
-          onPress={() => {}}
-          enabled={true}
-          loading={false}
-        />
-      </Footer>
-    </Container>
+    <KeyboardAvoidingView behavior="position" enabled>
+      <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+        <Container>
+          <StatusBar
+            barStyle="dark-content"
+            backgroundColor="transparent"
+            translucent
+          />
+          <Header>
+            <Title>Estamos {"\n"} quase la.</Title>
+            <SubTitle>
+              Faca seu login para comecar {"\n"}
+              uma experiencia incrivel.
+            </SubTitle>
+          </Header>
+          <Form>
+            <Input
+              iconName="mail"
+              keyboardType="email-address"
+              placeholder="E-mail"
+              autoCapitalize="none"
+              autoCorrect={false}
+              value={email}
+              onChangeText={setEmail}
+            />
+            <PasswordInput
+              iconName="lock"
+              placeholder="Senha"
+              onChangeText={setPassword}
+              value={password}
+            />
+          </Form>
+          <Footer>
+            <Button
+              title="Login"
+              enabled
+              loading={false}
+              onPress={handleSignIn}
+            />
+            <Button
+              title="Criar conta gratuita"
+              color={"#252525"}
+              light={false}
+              onPress={() => {}}
+              enabled={true}
+              loading={false}
+            />
+          </Footer>
+        </Container>
+      </TouchableWithoutFeedback>
+    </KeyboardAvoidingView>
   );
 }
